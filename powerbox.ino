0bcdfd6a6b00d55a1e8a1dbf11e8969bc436d1bb
Reed Morse <reed.morse@gmail.com>
2018-08-05 13:20:40 -0700

    buffer reads

--- powerbox.ino
+++ powerbox.ino
@@ -1,33 +1,53 @@
+#include <stdint.h>
 #include <Adafruit_NeoPixel.h>
 
 #define PIN_ONBOARD_LED 13
 #define PIN_NEOPIXEL 6
 #define MV_PER_AMP 136
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(250, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel _strip = Adafruit_NeoPixel(250, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// Sensors start at A0.
+#define NUM_SENSORS 6
+#define BUFFER_SIZE 16
+
+int _bufferFrame = 0;
+double _buffer[NUM_SENSORS][BUFFER_SIZE] = { 0 };
 
 void setup() {
   pinMode(PIN_ONBOARD_LED , OUTPUT);
   digitalWrite(PIN_ONBOARD_LED , HIGH);
 
-  strip.begin();
-  strip.show();
+  _strip.begin();
+  _strip.show();
   
   Serial.begin(9600);
 }
 
 void loop() {
-  for(int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, 255);
+  for(int i = 0; i < _strip.numPixels(); i++) {
+    _strip.setPixelColor(i, 255);
   }
-  strip.show();
+  _strip.show();
 
-  for (int sensor = 0; sensor < 6; sensor++) {
-    double amperage = readACS711(sensor);
-    Serial.printf("A%d: %f\t", sensor, amperage);
+  // Fill the current frame of the buffer.
+  for (int sensor = 0; sensor < NUM_SENSORS; sensor++) {
+    _buffer[sensor][_bufferFrame] = readACS711(sensor);
   }
+  _bufferFrame += 1;
+  if (_bufferFrame > BUFFER_SIZE - 1) { _bufferFrame = 0; }
+
+  // Average the buffer and read it out.
+  for (int sensor = 0; sensor < NUM_SENSORS; sensor++) {
+    double average = 0;
+    for (int frame = 0; frame < BUFFER_SIZE; frame++) {
+       average += _buffer[sensor][frame];
+    }
+    average = average / BUFFER_SIZE;
+    Serial.printf("A%d: %f\t", sensor, average);
+  }
+
   Serial.println("");
-
   delay(500);
 }
 
